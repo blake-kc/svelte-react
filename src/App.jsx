46ac18e60fbd47087324bdceceb07660e676e3ea
Blake Netzeband <blake.netzeband@kinandcarta.com>
2023-02-15 15:41:14 -0700

    added deps. config tailwind and postcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useRef, useLayoutEffect } from "react";
+import Hello from "./Hello.svelte";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const svelteRef = useRef();
+
+  useLayoutEffect(() => {
+    // remove svelte instance if exists
+    while (svelteRef.current?.firstChild) {
+      svelteRef.current.firstChild.remove();
+    }
+
+    new Hello({ target: svelteRef.current });
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <div ref={svelteRef}></div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
