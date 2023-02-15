68fecc7c6add18659f9ed9c67f6bec2de3ee98cd
Blake Netzeband <blake.netzeband@kinandcarta.com>
2023-02-15 15:50:23 -0700

    create svelte component generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
 import { useState, useRef, useLayoutEffect } from "react";
 import Hello from "./Hello.svelte";
 
-import "./App.css";
+function generateSvelteComponent(Component) {
+  return (props) => {
+    const svelteRef = useRef();
+
+    useLayoutEffect(() => {
+      // remove svelte instance if exists
+      while (svelteRef.current?.firstChild) {
+        svelteRef.current.firstChild.remove();
+      }
+
+      new Component({
+        target: svelteRef.current,
+        props,
+      });
+    }, []);
+
+    return <div ref={svelteRef}></div>;
+  };
+}
+
+const SvelteHello = generateSvelteComponent(Hello);
 
 function App() {
   const [count, setCount] = useState(0);
-  const svelteRef = useRef();
-
-  useLayoutEffect(() => {
-    // remove svelte instance if exists
-    while (svelteRef.current?.firstChild) {
-      svelteRef.current.firstChild.remove();
-    }
-
-    new Hello({
-      target: svelteRef.current,
-      props: {
-        text: "from react",
-        onClick: () => alert("Click from react"),
-      },
-    });
-  }, []);
 
   return (
     <div>
-      <div ref={svelteRef}></div>
+      <SvelteHello />
+      <SvelteHello
+        text="from react!"
+        onClick={() => setCount((count) => ++count)}
+      />
       <button
         className="btn btn-success"
         onClick={() => setCount((count) => count + 1)}
